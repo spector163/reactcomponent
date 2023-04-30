aa821209cf70fe05c92dbf0c380b3ef192296041
naerndra <narendra@collegebatch.com>
2023-04-30 13:31:20 +0530

    working on it

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-
-import './App.css'
+import Accordian from "./components/Accordian";
 
 function App() {
-
-  return (
-    <main>
-     <h1>starting fresh!!</h1>
-    </main>
-  )
+	return (
+		<main className='grid place-items-center mt-3 gap-2'>
+			<Accordian></Accordian>
+		</main>
+	);
 }
 
-export default App
+export default App;
